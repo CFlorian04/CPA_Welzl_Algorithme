45be801ce6c8f7c367f9ec5ad5d714fc017e37d7
CFlorian04 <113207681+CFlorian04@users.noreply.github.com>
2023-12-23 12:36:48 +0100

    Version Finale

--- CPA_PROJET/src/algorithms/DefaultTeam.java
+++ CPA_PROJET/src/algorithms/DefaultTeam.java
@@ -17,6 +17,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Stack;
 import java.util.function.Supplier;
 
@@ -50,13 +51,14 @@ public class DefaultTeam {
     deleteFileContent(1);
 
     for (int i = 0; i < nombreDeTest; i++) {
+      RefreshPoint();
       cercle = measureExecutionTime(0, (Supplier<Circle>) () -> CercleMin(points));
+      cercle = measureExecutionTime(1, (Supplier<Circle>) () -> WelzlAlgorithme(points, new ArrayList<Point>()));
       RefreshPoint();
     }
 
-    for (int i = 0; i < nombreDeTest; i++) {
-      cercle = measureExecutionTime(1, (Supplier<Circle>) () -> WelzlAlgorithme(points));
-      RefreshPoint();
+    if (cercle == null) {
+      cercle = new Circle(new Point(0, 0), 0);
     }
 
     return cercle;
@@ -115,52 +117,13 @@ public class DefaultTeam {
     Point p = antipodales.get(0).getP();
     Point q = antipodales.get(0).getQ();
 
-    for (Line a : antipodales)
-      if (a.getP().distance(a.getQ()) > p.distance(q)) {
-        p = a.getP();
-        q = a.getQ();
+    for (Line pointA : antipodales)
+      if (pointA.getP().distance(pointA.getQ()) > p.distance(q)) {
+        p = pointA.getP();
+        q = pointA.getQ();
       }
 
     return new Line(p, q);
-  }
-
-  // Fonction d'appel de la fonction de calcul de base du cercle minimum
-  private Circle CercleMin(ArrayList<Point> points) {
-
-    if (points.size() < 1)
-      return null;
-
-    ArrayList<Point> rest = (ArrayList<Point>) points.clone();
-    Point dummy = rest.get(0);
-    Point p = dummy;
-    for (Point s : rest)
-      if (dummy.distance(s) > dummy.distance(p))
-        p = s;
-    Point q = p;
-    for (Point s : rest)
-      if (p.distance(s) > p.distance(q))
-        q = s;
-    double cX = .5 * (p.x + q.x);
-    double cY = .5 * (p.y + q.y);
-    double cRadius = .5 * p.distance(q);
-    rest.remove(p);
-    rest.remove(q);
-    while (!rest.isEmpty()) {
-      Point s = rest.remove(0);
-      double distanceFromCToS = Math.sqrt((s.x - cX) * (s.x - cX) + (s.y - cY) * (s.y - cY));
-      if (distanceFromCToS <= cRadius)
-        continue;
-      double cPrimeRadius = .5 * (cRadius + distanceFromCToS);
-      double alpha = cPrimeRadius / (double) (distanceFromCToS);
-      double beta = (distanceFromCToS - cPrimeRadius) / (double) (distanceFromCToS);
-      double cPrimeX = alpha * cX + beta * s.x;
-      double cPrimeY = alpha * cY + beta * s.y;
-      cRadius = cPrimeRadius;
-      cX = cPrimeX;
-      cY = cPrimeY;
-    }
-
-    return new Circle(new Point((int) cX, (int) cY), (int) cRadius);
   }
 
   // Fonction d'appel de l'algorithme de Graham
@@ -213,7 +176,7 @@ public class DefaultTeam {
       for (int i = 1; i < enveloppe.size() - 1; i++) {
         double angle = calculerAngle(enveloppe.get(i - 1), enveloppe.get(i), enveloppe.get(i + 1));
         if (angle < 180) {
-          System.out.println("Turn " + turn + " / Number " + i + " : " + angle);
+          // System.out.println("Turn " + turn + " / Number " + i + " : " + angle);
           enveloppe.remove(i);
           toQuit = true;
           break;
@@ -225,82 +188,183 @@ public class DefaultTeam {
       }
     }
 
-    System.out.println(enveloppe.toString());
+    // System.out.println(enveloppe.toString());
     return enveloppe;
   }
 
-  // Fonction d'appel de l'algorithme de Welzl
-  private Circle WelzlAlgorithme(ArrayList<Point> points) {
-    Circle cercle = null;
+  // Fonction d'appel de la fonction de calcul de base du cercle minimum
+  private Circle CercleMinOptimise(ArrayList<Point> points) {
 
-    ArrayList<Point> pointsMelanges = new ArrayList<>(points);
-    Collections.shuffle(pointsMelanges);
+    if (points.size() < 1)
+      return null;
 
-    cercle = WelzlRecursif(pointsMelanges, new ArrayList<>());
+    ArrayList<Point> rest = (ArrayList<Point>) points.clone();
+    Point dummy = rest.get(0);
+    Point p = dummy;
+    for (Point s : rest)
+      if (dummy.distance(s) > dummy.distance(p))
+        p = s;
+    Point q = p;
+    for (Point s : rest)
+      if (p.distance(s) > p.distance(q))
+        q = s;
+    double cX = .5 * (p.x + q.x);
+    double cY = .5 * (p.y + q.y);
+    double cRadius = .5 * p.distance(q);
+    rest.remove(p);
+    rest.remove(q);
+    while (!rest.isEmpty()) {
+      Point s = rest.remove(0);
+      double distanceFromCToS = Math.sqrt((s.x - cX) * (s.x - cX) + (s.y - cY) * (s.y - cY));
+      if (distanceFromCToS <= cRadius)
+        continue;
+      double cPrimeRadius = .5 * (cRadius + distanceFromCToS);
+      double alpha = cPrimeRadius / (double) (distanceFromCToS);
+      double beta = (distanceFromCToS - cPrimeRadius) / (double) (distanceFromCToS);
+      double cPrimeX = alpha * cX + beta * s.x;
+      double cPrimeY = alpha * cY + beta * s.y;
+      cRadius = cPrimeRadius;
+      cX = cPrimeX;
+      cY = cPrimeY;
+    }
 
-    return cercle;
+    return new Circle(new Point((int) cX, (int) cY), (int) cRadius);
   }
+
+  // Fonction d'appel de l'algorithme de Welzl
+  public Circle WelzlAlgorithme(ArrayList<Point> P, ArrayList<Point> R) {
+    ArrayList<Point> P1 = new ArrayList<Point>(P);
+    Random rand = new Random(); // point au hasard
+    Circle d = new Circle(new Point(0, 0), 0);
+    if (P1.isEmpty() || R.size() == 3) {
+      d = WelzlMinimumCircle(new ArrayList<Point>(), R);
+    } else {
+      Point pt = P1.get(rand.nextInt(P1.size()));
+      P1.remove(pt);
+      d = WelzlAlgorithme(P1, R);
+      if (d != null && !circleContains(d, pt)) {
+        R.add(pt);
+        d = WelzlAlgorithme(P1, R);
+        R.remove(pt);
+      }
+    }
+    return d;
+  }
+
+  private Circle CercleMin(ArrayList<Point> inputPoints){
+    ArrayList<Point> points = (ArrayList<Point>) inputPoints.clone();
+    if (points.size()<1) return null;
+    double cX,cY,cRadius,cRadiusSquared;
+    for (Point p: points){
+        for (Point q: points){
+            cX = .5*(p.x+q.x);
+            cY = .5*(p.y+q.y);
+            cRadiusSquared = 0.25*((p.x-q.x)*(p.x-q.x)+(p.y-q.y)*(p.y-q.y));
+            boolean allHit = true;
+            for (Point s: points)
+                if ((s.x-cX)*(s.x-cX)+(s.y-cY)*(s.y-cY)>cRadiusSquared){
+                    allHit = false;
+                    break;
+                }
+            if (allHit) return new Circle(new Point((int)cX,(int)cY),(int)Math.sqrt(cRadiusSquared));
+        }
+    }
+    double resX=0;
+    double resY=0;
+    double resRadiusSquared=Double.MAX_VALUE;
+    for (int i=0;i<points.size();i++){
+        for (int j=i+1;j<points.size();j++){
+            for (int k=j+1;k<points.size();k++){
+                Point p=points.get(i);
+                Point q=points.get(j);
+                Point r=points.get(k);
+                //si les trois sont colineaires on passe
+                if ((q.x-p.x)*(r.y-p.y)-(q.y-p.y)*(r.x-p.x)==0) continue;
+                //si p et q sont sur la meme ligne, ou p et r sont sur la meme ligne, on les echange
+                if ((p.y==q.y)||(p.y==r.y)) {
+                    if (p.y==q.y){
+                        p=points.get(k); //ici on est certain que p n'est sur la meme ligne de ni q ni r
+                        r=points.get(i); //parce que les trois points sont non-colineaires
+                    } else {
+                        p=points.get(j); //ici on est certain que p n'est sur la meme ligne de ni q ni r
+                        q=points.get(i); //parce que les trois points sont non-colineaires
+                    }
+                }
+                //on cherche les coordonnees du cercle circonscrit du triangle pqr
+                //soit m=(p+q)/2 et n=(p+r)/2
+                double mX=.5*(p.x+q.x);
+                double mY=.5*(p.y+q.y);
+                double nX=.5*(p.x+r.x);
+                double nY=.5*(p.y+r.y);
+                //soit y=alpha1*x+beta1 l'equation de la droite passant par m et perpendiculaire a la droite (pq)
+                //soit y=alpha2*x+beta2 l'equation de la droite passant par n et perpendiculaire a la droite (pr)
+                double alpha1=(q.x-p.x)/(double)(p.y-q.y);
+                double beta1=mY-alpha1*mX;
+                double alpha2=(r.x-p.x)/(double)(p.y-r.y);
+                double beta2=nY-alpha2*nX;
+                //le centre c du cercle est alors le point d'intersection des deux droites ci-dessus
+                cX=(beta2-beta1)/(double)(alpha1-alpha2);
+                cY=alpha1*cX+beta1;
+                cRadiusSquared=(p.x-cX)*(p.x-cX)+(p.y-cY)*(p.y-cY);
+                if (cRadiusSquared>=resRadiusSquared) continue;
+                boolean allHit = true;
+                for (Point s: points)
+                    if ((s.x-cX)*(s.x-cX)+(s.y-cY)*(s.y-cY)>cRadiusSquared){
+                        allHit = false;
+                        break;
+                    }
+                if (allHit) {System.out.println("Found r="+Math.sqrt(cRadiusSquared));resX=cX;resY=cY;resRadiusSquared=cRadiusSquared;}
+            }
+        }
+    }
+    return new Circle(new Point((int)resX,(int)resY),(int)Math.sqrt(resRadiusSquared));
+}
+
 
   /***************************************
    * ALGORITHME DE WELZL - CERCLE MINIMUM
    ****************************************/
 
-  // Fonction récursive pour trouver le cercle minimum
-  private Circle WelzlRecursif(List<Point> points, List<Point> boundary) {
-    if (points.isEmpty() || boundary.size() == 3) {
-      return WelzlMinimumCircle(boundary);
+  // Calcul du cercle minimum pour l'algorithme de Welzl
+  public Circle WelzlMinimumCircle(ArrayList<Point> P, ArrayList<Point> R) {
+    if (P.isEmpty() && R.size() == 0)
+      return new Circle(new Point(0, 0), 0);
+    Circle D = new Circle(new Point(0, 0), 0);
+    if (R.size() == 1) {
+      D = new Circle(R.get(0), 0);
     }
-
-    Point randomPoint = points.remove(0);
-    Circle circle = WelzlRecursif(points, boundary);
-
-    if (circle != null && !circleContains(circle, randomPoint)) {
-      boundary.add(randomPoint);
-      circle = WelzlRecursif(points, boundary);
-      boundary.remove(randomPoint);
+    if (R.size() == 2) {
+      double cx = (R.get(0).x + R.get(1).x) / 2;
+      double cy = (R.get(0).y + R.get(1).y) / 2;
+      double d = R.get(0).distance(R.get(1)) / 2;
+      Point p = new Point((int) cx, (int) cy);
+      D = new Circle(p, (int) Math.ceil(d));
+    } else {
+      if (R.size() == 3)
+        D = WelzlCircle3Points(R.get(0), R.get(1), R.get(2));
     }
-
-    points.add(0, randomPoint);
-    return circle;
+    return D;
   }
 
-  // Calcul du cercle minimum pour l'algorithme de Welzl
-  private static Circle WelzlMinimumCircle(List<Point> points) {
-    if (points.isEmpty()) {
-      return null;
-    } else if (points.size() == 1) {
-      // Cas avec un seul point
-      return new Circle(points.get(0), 0);
-    } else if (points.size() == 2) {
-      // Cas avec deux points
-      Point p1 = points.get(0);
-      Point p2 = points.get(1);
-      Point center = new Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
-      int radius = (int) Math.ceil(p1.distance(p2) / 2);
-      return new Circle(center, radius);
-    } else {
-      // Autres Cas
-      Point pointA = points.get(0);
-      Point pointB = points.get(1);
-      Point pointC = points.get(2);
 
-      // Calcul du centre et du rayon du cercle
-      double d = 2 * (pointA.x * (pointB.y - pointC.y) + pointB.x * (pointC.y - pointA.y)
-          + pointC.x * (pointA.y - pointB.y));
+  // Calcul du cercle minimum avec 3 points pour l'algorithme de Welzl
+  private Circle WelzlCircle3Points(Point pointA, Point pointB, Point pointC) {
+    double d = (pointA.x * (pointB.y - pointC.y) + pointB.x * (pointC.y - pointA.y) + pointC.x * (pointA.y - pointB.y))
+        * 2;
+    if (d == 0)
+      return new Circle(new Point(0, 0), 0);
 
-      double ux = ((pointA.x * pointA.x + pointA.y * pointA.y) * (pointB.y - pointC.y)
-          + (pointB.x * pointB.x + pointB.y * pointB.y) * (pointC.y - pointA.y)
-          + (pointC.x * pointC.x + pointC.y * pointC.y) * (pointA.y - pointB.y)) / d;
+    double x = ((calculNormeVecteur(pointA) * (pointB.y - pointC.y))
+        + (calculNormeVecteur(pointB) * (pointC.y - pointA.y))
+        + (calculNormeVecteur(pointC) * (pointA.y - pointB.y))) / d;
 
-      double uy = ((pointA.x * pointA.x + pointA.y * pointA.y) * (pointC.x - pointB.x)
-          + (pointB.x * pointB.x + pointB.y * pointB.y) * (pointA.x - pointC.x)
-          + (pointC.x * pointC.x + pointC.y * pointC.y) * (pointB.x - pointA.x)) / d;
+    double y = ((calculNormeVecteur(pointA) * (pointC.x - pointB.x))
+        + (calculNormeVecteur(pointB) * (pointA.x - pointC.x))
+        + (calculNormeVecteur(pointC) * (pointB.x - pointA.x))) / d;
 
-      Point center = new Point((int) ux, (int) uy);
-      int radius = (int) Math.ceil(
-          Math.max(Math.max(pointA.distance(center), pointB.distance(center)), pointC.distance(center)));
-      return new Circle(center, radius);
-    }
+    Point p = new Point((int) x, (int) y);
+
+    return new Circle(p, (int) Math.ceil(p.distance(pointA)));
   }
 
   /**************************************
@@ -308,15 +372,15 @@ public class DefaultTeam {
    ***************************************/
 
   // Calcul du produit vectoriel [pq] [st]
-  private double produitVectoriel(Point p, Point q, Point s, Point t) {
+  private double calculProduitVectoriel(Point p, Point q, Point s, Point t) {
     return ((q.x - p.x) * (t.y - s.y) - (q.y - p.y) * (t.x - s.x));
   }
 
   // Calcul de l'angle [ab] [bc]
-  private static double calculerAngle(Point a, Point b, Point c) {
+  private static double calculerAngle(Point pointA, Point pointB, Point pointC) {
 
-    double angleRad = Math.atan2(c.getY() - b.getY(), c.getX() - b.getX())
-        - Math.atan2(a.getY() - b.getY(), a.getX() - b.getX());
+    double angleRad = Math.atan2(pointC.getY() - pointB.getY(), pointC.getX() - pointB.getX())
+        - Math.atan2(pointA.getY() - pointB.getY(), pointA.getX() - pointB.getX());
     angleRad = (angleRad + 2 * Math.PI) % (2 * Math.PI);
 
     return Math.toDegrees(angleRad);
@@ -328,8 +392,8 @@ public class DefaultTeam {
   }
 
   // Calcul de la distance entre trois points
-  private double distanceBetween3Points(Point p, Point a, Point b) {
-    return Math.abs(produitVectoriel(a, b, a, p));
+  private double distanceBetween3Points(Point p, Point pointA, Point pointB) {
+    return Math.abs(calculProduitVectoriel(pointA, pointB, pointA, p));
   }
 
   // Calcul de deux points opposés par rapport au plan
@@ -362,19 +426,25 @@ public class DefaultTeam {
     return antipodals;
   }
 
+  // Calcul de la norme d'un vecteur
+  private int calculNormeVecteur(Point pointA) {
+    return (int) (Math.pow(pointA.x, 2) + Math.pow(pointA.y, 2));
+  }
+
   /****************************************************************************
    * FONCTIONS POUR MESURER LE TEMPS D'EXECUTION ET POUR STOCKER LES RÉSULTATS
    *****************************************************************************/
-  
+
   // Mesurer le temps d'execution de la fonction
   private static <T> T measureExecutionTime(int AlgoType, Supplier<T> function) {
-    long startTime = System.nanoTime();
+    long startTime = System.nanoTime() / 1000; // Convertir en microsecondes
     T result = function.get();
-    long endTime = System.nanoTime();
+    long endTime = System.nanoTime() / 1000; // Convertir en microsecondes
     long duration = endTime - startTime;
+    System.out.println("Execution time: " + duration);
     storeExecutionTime(AlgoType, duration);
     return result;
-  }
+}
 
   // Écrit le temps d'execution de la fonction dans le fichier
   private static void storeExecutionTime(int AlgoType, long duration) {
@@ -416,9 +486,9 @@ public class DefaultTeam {
   private static String GetFileName(int AlgoType) {
     String FileName = new String();
     if (AlgoType == 0) { // Algo de base
-      FileName = "default_times.txt";
+      FileName = "times_default.txt";
     } else if (AlgoType == 1) { // Algo de Welzl
-      FileName = "welzl_times.txt";
+      FileName = "times_welzl.txt";
     }
     return FileName;
   }
